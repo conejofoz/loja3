a7efb30b40b44b5ce8bf65681432514f73244ace
geral <geral@192.168.217.131>
2018-02-20 14:01:45 -0300

    maquetando slide ok

--- vistas/modulos/slide.php
+++ vistas/modulos/slide.php
@@ -14,13 +14,13 @@ SLIDE SHOW
                 <img src="http://localhost/backend/vistas/img/slide/default/back_default.jpg">
                 <div class="slideOpciones slideOpcion1">
                     
-                    <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide1/calzado.png">
+                    <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide1/calzado.png" style="top:15%; right:10%; width:45%">
                     
-                    <div class="textosSlide">
+                    <div class="textosSlide" style="top:20%; left:10%; width:40%">
                         
-                        <h1>Lorem Ipsum</h1>
-                        <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
-                        <h3>Lorem Ipsum</h3>
+                        <h1 style="color:#333">Lorem Ipsum</h1>
+                        <h2 style="color:#777">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+                        <h3 style="color:#888">Lorem Ipsum</h3>
                         <a href="#">
                             <button class="btn btn-default backColor text-uppercase">
                                 VER PRODUCTO <span class="fa fa-chevron-right"></span>
@@ -37,13 +37,59 @@ SLIDE SHOW
                 <img src="http://localhost/backend/vistas/img/slide/default/back_default.jpg">
                 <div class="slideOpciones slideOpcion2">
                     
-                    <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide2/curso.png">
+                    <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide2/curso.png" style="top:5%; left:15%; width:25%">
                     
-                    <div class="textosSlide">
+                    <div class="textosSlide" style="top:20%; right:10%; width:40%">
                         
-                        <h1>Lorem Ipsum</h1>
-                        <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
-                        <h3>Lorem Ipsum</h3>
+                        <h1 style="color:#333">Lorem Ipsum</h1>
+                        <h2 style="color:#777">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+                        <h3 style="color:#888">Lorem Ipsum</h3>
+                        <a href="#">
+                            <button class="btn btn-default backColor text-uppercase">
+                                VER PRODUCTO <span class="fa fa-chevron-right"></span>
+                            </button> 
+                        </a>
+                    </div>
+                    
+                </div>
+            </li>
+            <li>
+                
+                <!--SLIDE 3 -->
+                
+                <img src="http://localhost/backend/vistas/img/slide/slide3/fondo2.jpg">
+                <div class="slideOpciones slideOpcion2">
+                    
+                    <img class="imgProducto" src="http://localhost/backend/vistas/img/slide/slide3/iphone.png" style="top:5%; left:15%; width:25%">
+                    
+                    <div class="textosSlide" style="top:20%; right:10%; width:40%">
+                        
+                        <h1 style="color:#eee">Lorem Ipsum</h1>
+                        <h2 style="color:#ccc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+                        <h3 style="color:#aaa">Lorem Ipsum</h3>
+                        <a href="#">
+                            <button class="btn btn-default backColor text-uppercase">
+                                VER PRODUCTO <span class="fa fa-chevron-right"></span>
+                            </button> 
+                        </a>
+                    </div>
+                    
+                </div>
+            </li>
+            <li>
+                
+                <!--SLIDE 4 -->
+                
+                <img src="http://localhost/backend/vistas/img/slide/slide4/fondo3.jpg">
+                <div class="slideOpciones slideOpcion1">
+                    
+                    <img class="imgProducto" src="" style="top:5%; right:15%; width:25%">
+                    
+                    <div class="textosSlide" style="top:20%; left:10%; width:40%">
+                        
+                        <h1 style="color:#333">Lorem Ipsum</h1>
+                        <h2 style="color:#777">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </h2>
+                        <h3 style="color:#888">Lorem Ipsum</h3>
                         <a href="#">
                             <button class="btn btn-default backColor text-uppercase">
                                 VER PRODUCTO <span class="fa fa-chevron-right"></span>
@@ -54,6 +100,22 @@ SLIDE SHOW
                 </div>
             </li>
         </ul>
+        <!--===============
+        paginacion
+        ================-->
+        <ol id="paginacion">
+            <li item="1"><span class="fa fa-circle"></span></li>
+            <li item="2"><span class="fa fa-circle"></span></li>
+            <li item="3"><span class="fa fa-circle"></span></li>
+            <li item="4"><span class="fa fa-circle"></span></li>
+        </ol>
+        
+        
+        <!--===============
+        flechas
+        ================-->
+        <div class="flechas" id="retroceder"><span class="fa fa-chevron-left"></span></div>
+        <div class="flechas" id="avanzar"><span class="fa fa-chevron-right"></span></div>
     </div>
 
 </div>
