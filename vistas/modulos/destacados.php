da4bb01c6d6fd0c950eb599b3050838210bf729f
conej <conej@DESKTOP-F0Q0UOR.local>
2018-02-22 21:10:12 -0300

    vitrine de produtos gratis ok

--- vistas/modulos/destacados.php
+++ vistas/modulos/destacados.php
@@ -3,7 +3,7 @@ BANNER
 ==========================================================-->
 <figure class="banner">
     <img src="http://localhost/backend/vistas/img/banner/default.jpg" class="img-responsive" width="100%">
-    
+
     <div class="textoBanner textoDer">
         <h1 style="color:#fff">OFERTAS ESPECIALES</h1>
         <h2 style="color:#fff"><strong>50% off</strong></h2>
@@ -21,7 +21,7 @@ BARRA PRODUTOS GRATIS
     <div class="container">
         <div class="row">
             <div class="col-xs-12 organizarProductos">
-                
+
                 <div class="btn-group pull-right">
                     <button type="button" class="btn btn-default btnGrid" id="btnGrid0">
                         <i class="fa fa-th" aria-hidden="true"></i>
@@ -32,10 +32,255 @@ BARRA PRODUTOS GRATIS
                         <span class="col-xs-0 pull-right">LISTA</span>
                     </button>
                 </div>
-                
+
             </div> <!--fim 12 colunas-->
-            
+
         </div><!--fim row-->
     </div><!--fim container-->
-    
+
+</div>
+
+
+
+
+
+<!--=========================================================
+VITRINE DE PRODUTOS GRATIS
+==========================================================-->
+<div class="container-fluid productos">
+    <div class="container">
+        <div class="row">
+            <!--BARRA DE TITULO-->
+            <div class="col-xs-12 tituloDestacado">
+                <!--=========================================-->
+                <div class="col-sm-6 col-xs-12">
+                    <h1><small>ARTICULOS GRATUITOS</small></h1>
+                </div>
+
+
+                <!--=========================================-->
+                <div class="col-sm-6 col-xs-12">
+                    <a href="articulos-gratis">
+                        <button class="btn btn-default backColor pull-right">
+                            VER MÁS <span class="fa fa-chevron-right"></span>
+                        </button>
+                    </a>
+                </div>
+
+
+
+                <!--=========================================-->
+
+
+            </div>
+
+            <hr>
+
+        </div><!--========FIM DE ROW================-->
+
+
+        <!--=========================================================
+        VITRINE DE PRODUTOS EM QUADRICULOS
+        ==========================================================-->
+        <ul class="grid0">
+            <!--Produto 1-->
+            <li class="col-md-3 col-sm-6 col-xs-12">
+                <!--==============-->
+                <figure>
+                    <a href="#" class="pixelSubCategorias">
+                        <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio04.jpg" class="img-responsive">
+                    </a>
+                </figure>
+                
+                
+                <!--==============-->
+                <h4>
+                    <small>
+                        <a href="#" class="pixelProducto">
+                            Collar de Diamantes
+                        </a>
+                    </small>
+                </h4>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 precio">
+                    <h2><small>GRATIS</small></h2>
+                </div>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 enlaces">
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            
+                        </button>
+                        
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                data-toggle="tooltip" title="Ver producto">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            
+                        </button>
+                        </a>
+                        
+                    </div>
+                </div>
+            </li>
+            
+            
+            
+            <!--Produto 2-->
+            <li class="col-md-3 col-sm-6 col-xs-12">
+                <!--==============-->
+                <figure>
+                    <a href="#" class="pixelSubCategorias">
+                        <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio03.jpg" class="img-responsive">
+                    </a>
+                </figure>
+                
+                
+                <!--==============-->
+                <h4>
+                    <small>
+                        <a href="#" class="pixelProducto">
+                            Bolso deportivo gris
+                        </a>
+                    </small>
+                </h4>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 precio">
+                    <h2><small>GRATIS</small></h2>
+                </div>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 enlaces">
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            
+                        </button>
+                        
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                data-toggle="tooltip" title="Ver producto">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            
+                        </button>
+                        </a>
+                        
+                    </div>
+                </div>
+            </li>
+            
+            
+            
+            <!--Produto 3-->
+            <li class="col-md-3 col-sm-6 col-xs-12">
+                <!--==============-->
+                <figure>
+                    <a href="#" class="pixelSubCategorias">
+                        <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio02.jpg" class="img-responsive">
+                    </a>
+                </figure>
+                
+                
+                <!--==============-->
+                <h4>
+                    <small>
+                        <a href="#" class="pixelProducto">
+                            Bolso Militar
+                        </a>
+                    </small>
+                </h4>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 precio">
+                    <h2><small>GRATIS</small></h2>
+                </div>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 enlaces">
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            
+                        </button>
+                        
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                data-toggle="tooltip" title="Ver producto">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            
+                        </button>
+                        </a>
+                        
+                    </div>
+                </div>
+            </li>
+            
+            
+            
+            <!--Produto 4-->
+            <li class="col-md-3 col-sm-6 col-xs-12">
+                <!--==============-->
+                <figure>
+                    <a href="#" class="pixelSubCategorias">
+                        <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio01.jpg" class="img-responsive">
+                    </a>
+                </figure>
+                
+                
+                <!--==============-->
+                <h4>
+                    <small>
+                        <a href="#" class="pixelProducto">
+                            Pulsera de diamantes
+                        </a>
+                    </small>
+                </h4>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 precio">
+                    <h2><small>GRATIS</small></h2>
+                </div>
+                
+                
+                <!--==============-->
+                <div class="col-xs-6 enlaces">
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            
+                        </button>
+                        
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                data-toggle="tooltip" title="Ver producto">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            
+                        </button>
+                        </a>
+                        
+                    </div>
+                </div>
+            </li>
+            
+            
+            
+        </ul>
+
+
+    </div>
 </div>
