cb102dfc5380cac568b763123cfdcadb31eb9d89
geral <geral@192.168.217.131>
2018-02-24 14:56:29 -0300

    vitrine de produtos em lista ok

--- vistas/modulos/destacados.php
+++ vistas/modulos/destacados.php
@@ -84,7 +84,7 @@ VITRINE DE PRODUTOS GRATIS
         <!--=========================================================
         VITRINE DE PRODUTOS EM QUADRICULOS
         ==========================================================-->
-        <ul class="grid0">
+        <ul class="grid0" style="display: none">
             <!--Produto 1-->
             <li class="col-md-3 col-sm-6 col-xs-12">
                 <!--==============-->
@@ -283,6 +283,231 @@ VITRINE DE PRODUTOS GRATIS
 
 
         </ul>
+        
+        
+        <!--=========================================================
+        VITRINE DE PRODUTOS EM LISTA
+        ==========================================================-->
+        <ul class="list0">
+            <!--PRODUTO 1-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio04.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
+
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">Collar de Diamantes</a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2><small>GRATIS</small></h2>
+               
+
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
+            
+            
+            
+            
+            
+            <!--PRODUTO 2-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio03.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
+
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">Bolso deportivo gris</a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2><small>GRATIS</small></h2>
+               
+
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
+            
+            
+            
+            
+            
+            <!--PRODUTO 3-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio02.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
+
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">Bolso Militar</a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2><small>GRATIS</small></h2>
+               
+
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
+            
+            
+            
+            
+            
+            <!--PRODUTO 4-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/accesorios/accesorio01.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
+
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">Pulsera de diamantes</a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2><small>GRATIS</small></h2>
+               
+
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
+        </ul>
+        
+        
+        
 
 
     </div>
@@ -363,7 +588,7 @@ VITRINE DE PRODUTOS MAS VENDIDOS
         <!--=========================================================
         VITRINE DE PRODUTOS EM QUADRICULOS
         ==========================================================-->
-        <ul class="grid1">
+        <ul class="grid1" style="display: none">
             <!--Produto 1-->
             <li class="col-md-3 col-sm-6 col-xs-12">
                 <!--==============-->
@@ -601,18 +826,94 @@ VITRINE DE PRODUTOS MAS VENDIDOS
                     </div>
                 </div>
             </li>
+        </ul>
+        
+        
+        
+        
+        <!--=========================================================
+        VITRINE DE PRODUTOS EM LISTA
+        ==========================================================-->  
+        <ul class="list1">
+            <!--PRODUTO 1-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/ropa/ropa03.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
 
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">
+                                Falda de Flores
+                                <span class="label label-warning">Nuevo</span>
+                                <span class="label label-warning">40% off</span>
+                            
+                            </a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2>
+                        <small>
+                            <strong class="oferta">USD $29</strong>
+                        </small>
+                        <small>$11</small>
+                    </h2>
+               
 
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
 
+                        </button>
 
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
 
-
-
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
         </ul>
 
 
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!--=========================================================
 BARRA PRODUTOS MAS VISTOS
 ==========================================================-->
@@ -680,7 +981,7 @@ VITRINE DE PRODUTOS MAS VISTOS
         <!--=========================================================
         VITRINE DE PRODUTOS EM QUADRICULOS
         ==========================================================-->
-        <ul class="grid1">
+        <ul class="grid2" style="display: none">
             <!--Produto 1-->
             <li class="col-md-3 col-sm-6 col-xs-12">
                 <!--==============-->
@@ -957,21 +1258,103 @@ VITRINE DE PRODUTOS MAS VISTOS
                     </div>
                 </div>
             </li>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </ul>
+        
+        
+        
+        
+        
+        
+        
+        
+        <!--=========================================================
+        VITRINE DE PRODUTOS EM LISTA
+        ==========================================================-->  
+        <ul class="list2">
+            <!--PRODUTO 1-->
+            <li class="col-xs-12">
+                <!--==============================================-->
+                <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                    <figure>
+                        <a href="#" class="pixelProducto">
+                            <img src="http://localhost/backend/vistas/img/productos/cursos/curso05.jpg" class="img-responsive">
+                        </a>
+                    </figure>
+                </div>
+                
+                
+
+                <!--==============================================-->
+                <div class="col-lg-10 col-md-7 col-sm-8 col-xs-12">
+                    <h1>
+                        <small>
+                            <a href="#" class="pixelProducto">
+                                Curso de Bootstrap
+ 
+                                <span class="label label-warning">90% off</span>
+                            
+                            </a>
+                        </small>
+                    </h1>
+                    <p class="text-muted">
+                        Existem muitas variações das passagens do Lorem Ipsum disponíveis, mas a maior parte sofreu alterações de alguma forma, pela injecção de humor, ou de palavras aleatórias que nem sequer parecem suficientemente credíveis. Se vai usar uma passagem do Lorem Ipsum, deve ter a certeza que não contém nada
+                    </p>
+                    
+                    
+                    <h2>
+                        <small>
+                            <strong class="oferta">USD $100</strong>
+                        </small>
+                        <small>$10</small>
+                    </h2>
+               
+
+                    <div class="btn-group pull-left enlaces">
+                        <button type="button" class="btn btn-default btn-xs deseos" 
+                                idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+                        
+                        <button type="button" class="btn btn-default btn-xs agregarCarrito" 
+                                idProducto="404" 
+                                imagen="http://localhost/backend/vistas/img/productos/cursos/curso05.jpg"
+                                titulo="Curso de Bootstrap"
+                                precio="10"
+                                tipo="virtual"
+                                peso="0"
+                                data-toggle="tooltip"
+                                title="Agregar al carrito de compras">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+                            <button type="button" class="btn btn-default btn-xs" 
+                                    data-toggle="tooltip" title="Ver producto">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!--==============================================-->
+                <div class="col-xs-12">
+                    <hr>
+                </div>
+                <!--==============================================-->
+            </li>
+        </ul>        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 
     </div>
