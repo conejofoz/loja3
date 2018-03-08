604944b66a17ed5a8ba5785433819b74c149bd89
conej <conej@DESKTOP-F0Q0UOR.local>
2018-03-08 19:29:02 -0300

    dados dinamicos pagina info produtos parte 1 ok

--- vistas/modulos/infoproducto.php
+++ vistas/modulos/infoproducto.php
@@ -33,10 +33,16 @@ INFOPRODUCTO
 <div class="container-fluid infoproducto">
     <div class="container">
         <div class="row">
-            <!--==========================
-            VIDOR DE PRODUCTOS
-            ============================-->
-            <div class="col-md-5 col-sm-6 col-xs-12 visorImg">
+
+            <?php
+            /* =================
+             * VISOR DE IMAGENS
+              ================= */
+            $item = "ruta";
+            $valor = $rutas[0];
+            $infoProducto = ControladorProductos::ctrMostrarInfoProducto($item, $valor);
+            if ($infoProducto["tipo"] == "fisico") {
+                echo '<div class="col-md-5 col-sm-6 col-xs-12 visorImg">
                 <figure class="visor">
                     <img id="lupa1" class="img-thumbnail" src="http://localhost/backend/vistas/img/multimedia/tennis-verde/img-01.jpg" alt="tennis verde 11">
                     <img id="lupa2" class="img-thumbnail" src="http://localhost/backend/vistas/img/multimedia/tennis-verde/img-02.jpg" alt="tennis verde 11">
@@ -65,11 +71,90 @@ INFOPRODUCTO
                         <!-- items mirrored twice, total of 12 -->
                     </ul>
                 </div>
-            </div>
+            </div>';
+            } else {
+                /* =================
+                 * VISOR DE VIDEOS
+                  ================= */
+                echo '<div class="col-sm-6 col-xs-12">'
+                . '<iframe class="videoPresentacion" src="https://www.youtube.com/embed/N4aY6yX-MaM?rel=0&autoplay=1" width="100%" frameborder="0" allowfullscreen></iframe>'
+                . '</div>';
+            }
+            ?>
+
+            
+
+
+
+
+
+
+
+
             <!--==========================
             PRODUTO
             ============================-->
-            <div class="col-md-7 col-sm-6 col-xs-12">
+            
+            <?php
+                if($infoProducto["tipo"] == "fisico"){
+                    echo '<div class="col-md-7 col-sm-6 col-xs-12">';
+                } else {
+                    echo '<div class="col-sm-6 col-xs-12">'; 
+                }
+            ?>
+            
+            
+
+
+                <!--==========================
+                CONTINUAR COMPRANDO
+                ============================-->
+                <div class="col-xs-6">
+                    <h6>
+                        <a href="javascript:history.back()" class="text-muted">
+                            <i class="fa fa-reply"></i>Continuar comprando
+                        </a>
+                    </h6> 
+
+                </div>
+                <!--==========================
+                COMPARTIR EN REDES SOCIALES
+                ============================-->
+                <div class="col-xs-6">
+                    <h6>
+                        <a href="#" class="dropdown-toggle pull-right text-muted" data-toggle="dropdown">
+                            <i class="fa fa-plus"></i>Compartir
+                        </a>
+                        <ul class="dropdown-menu pull-right compartirRedes">
+                            <li>
+                                <p class="btnFacebook">
+                                    <i class="fa fa-facebook"></i>
+                                    Facebook
+                                </p>
+                            </li>
+                            <li>
+                                <p class="btnGoogle">
+                                    <i class="fa fa-google"></i>
+                                    Google
+                                </p>
+                            </li>
+                        </ul>
+                    </h6> 
+
+                </div>
+
+                <div class="clearfix"></div>
+
+
+
+                <!--==========================
+                ESPACIO PARA O PRODUCTO
+                ============================-->
+
+
+
+
+
 
 
                 <!--==========================
