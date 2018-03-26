dce077b4b8724e78e830d8781e6066de9d2910a4
conej <conej@DESKTOP-F0Q0UOR.local>
2018-03-26 14:02:28 -0300

    perfil de usuario com dados dinamicos parte 2

--- vistas/modulos/perfil.php
+++ vistas/modulos/perfil.php
@@ -51,12 +51,12 @@ SESÍON PERFIL
                 <a data-toggle="tab" href="#perfil"><i class="fa fa-user"></i> EDITAR PERFIL</a>
             </li>
             <li>
-                <a href="<?php echo $url;?>ofertas"><i class="fa fa-star"></i> VER OFERTAS</a>
+                <a href="<?php echo $url; ?>ofertas"><i class="fa fa-star"></i> VER OFERTAS</a>
             </li>
         </ul>
 
         <div class="tab-content">
-            
+
             <!--==============================================
             COMPRAS
             ===============================================-->
@@ -64,7 +64,7 @@ SESÍON PERFIL
                 <h3>HOME</h3>
                 <p>Some content.</p>
             </div>
-            
+
 
             <!--==============================================
             LISTA DE DESEJOS
@@ -73,8 +73,8 @@ SESÍON PERFIL
                 <h3>Menu 1</h3>
                 <p>Some content in menu 1.</p>
             </div>
-            
-            
+
+
             <!--==============================================
             PERFIL DE USUÁRIO
             ===============================================-->
@@ -84,31 +84,86 @@ SESÍON PERFIL
                         <div class="col-md-3 col-sm-4 col-xs-12 text-center">
                             <br>
                             <figure id="imgPerfil">
-                                <?php 
-                                
-                                    if($_SESSION["modo"] == "directo"){
-                                        
-                                        if($_SESSION["foto"] != ""){
-                                            echo '<img src="'.$url.$_SESSION["foto"].'" class="img-thumbnail">';
-                                        } else {
-                                            echo '<img src="'.$servidor.'vistas/img/usuarios/default/anonymous.png" class="img-thumbnail">';
-                                        }
-                                        
+                                <?php
+                                if ($_SESSION["modo"] == "directo") {
+
+                                    if ($_SESSION["foto"] != "") {
+                                        echo '<img src="' . $url . $_SESSION["foto"] . '" class="img-thumbnail">';
                                     } else {
-                                       echo '<img src="'.$_SESSION["foto"].'" class="img-thumbnail">'; 
+                                        echo '<img src="' . $servidor . 'vistas/img/usuarios/default/anonymous.png" class="img-thumbnail">';
                                     }
-                                
+                                } else {
+                                    echo '<img src="' . $_SESSION["foto"] . '" class="img-thumbnail">';
+                                }
                                 ?>
-                                
+
                             </figure>
+                            <br>
+                            <?php
+                            if ($_SESSION["modo"] == "directo") {
+                                
+                                echo '<button class="btn btn-default">Cambiar foto de perfil</button>';
+
+                            }
+                            ?>
+
                         </div>
-                        <div class="col-md-9 col-sm-8 col-xs-12"></div>
-                        
+                        <div class="col-md-9 col-sm-8 col-xs-12">
+                            <?php
+                            
+                            if ($_SESSION["modo"] != "directo") {
+                                
+                                echo '<br><label class="control-label text-muted text-uppercase" for="editarNombre">Nombre:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input type="text" class="form-control" name="editarNombre" value="'.$_SESSION["nombre"].'" readonly>
+                                    </div><br>';
+                                echo '<br><label class="control-label text-muted text-uppercase" for="editarEmail">Correo electrónico:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                        <input type="text" class="form-control" name="editarEmail" value="'.$_SESSION["email"].'" readonly>
+                                    </div><br>';
+                                echo '<br><label class="control-label text-muted text-uppercase">Modo de registro en el sistema:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="fa fa-'.$_SESSION["modo"].'"></i></span>
+                                        <input type="text" class="form-control" name="editarEmail" value="'.$_SESSION["modo"].'" readonly>
+                                    </div><br>';
+
+                            } else {
+                                
+                                echo '<br><label class="control-label text-muted text-uppercase" for="editarNombre">Cambiar Nombre:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                        <input type="text" class="form-control" id="editarNombre" name="editarNombre" value="'.$_SESSION["nombre"].'">
+                                    </div><br>';
+                                echo '<label class="control-label text-muted text-uppercase" for="editarEmail">Cambiar Email:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                                        <input type="email" class="form-control" id="editarEmail" name="editarEmail" value="'.$_SESSION["email"].'">
+                                    </div><br>';
+                                echo '<label class="control-label text-muted text-uppercase" for="editarPassword">Cambiar Contraseña:</label>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                        <input type="text" class="form-control" id="editarPassword" name="editarPassword" placeholder="Escribe la nueva contraseña">
+                                    </div><br>';
+                                echo '<button type="submit" class="btn btn-default backColor btn-md pull-left">Actualizar Datos</button>';
+                            }
+                            ?>
+                        </div>
+
                     </form>
+                    <button class="btn btn-danger btn-md pull-right">Eliminar Cuenta</button>
                 </div>
             </div>
-            
-            
+
+
         </div>
     </div>
 </div>
+<label class="control-label text-muted text-uppercase" for="editarNombre">Nombre:</label>
+<div class="input-group">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+    <input type="text" class="form-control" id="editarNombre" name="editar" value="" readonly >
+</div>
+<br>
+
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -46,6 +46,7 @@
         <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>vistas/css/slide.css">
         <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>vistas/css/productos.css">
         <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>vistas/css/infoproducto.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>vistas/css/perfil.css">
         
         <!--=========================================================
         PLUGINS DE JAVASCRIPT
