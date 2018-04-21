ed29a647e3036bfeaa721ae119236b8f3e55818f
conej <conej@DESKTOP-JEI7CDR.local>
2018-04-20 22:13:13 -0300

    capturando os dados del comprador

--- modelos/carrito.modelo.php
+++ modelos/carrito.modelo.php
@@ -16,11 +16,17 @@ class ModeloCarrito{
     
     
     static public function mdlNuevasCompras($tabla, $datos) {
+        
         $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla "
-                . "id_usuario, id_producto) "
-                . "VALUES(:id_usuario, :id_producto) ");
+                . "id_usuario, id_producto, metodo, email, direccion, pais) "
+                . "VALUES(:id_usuario, :id_producto, :metodo, :email, :direccion, :pais) ");
+        
         $stmt->bindParam(":id_usuario", $datos["idUsuario"], PDO::PARAM_INT);
         $stmt->bindParam(":id_producto", $datos["idProducto"], PDO::PARAM_INT);
+        $stmt->bindParam(":metodo", $datos["metodo"], PDO::PARAM_STR);
+        $stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
+        $stmt->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
+        $stmt->bindParam(":pais", $datos["pais"], PDO::PARAM_STR);
         $stmt->execute();
         if($stmt->execute()){
             return "ok";
